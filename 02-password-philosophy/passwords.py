ff6d2e1ef215e3d2d7dcfef3a41744c3ec21e3bf
Robby Stokoe <robbystk@gmail.com>
2020-12-02 09:27:41 -0700

    Tidy up in preparation for part two

--- 02-password-philosophy/passwords.py
+++ 02-password-philosophy/passwords.py
@@ -7,13 +7,22 @@ def input():
         for line in f:
             yield line
 
-def policy_pattern(policy_str):
-    PATTERN_TEMPLATE = Template('^([^$letter]*$letter){$min_occurrences,$max_occurrences}[^$letter]*$$')
-
+def parse_policy(policy_str):
     (occurrences, _, letter) = policy_str.partition(' ')
     (min_occurrences, _, max_occurrences) = occurrences.partition('-')
 
+    return (letter, min_occurrences, max_occurrences)
+
+def policy_pattern(policy_str):
+    PATTERN_TEMPLATE = Template('^([^$letter]*$letter){$min_occurrences,$max_occurrences}[^$letter]*$$')
+
+    (letter, min_occurrences, max_occurrences) = parse_policy(policy_str)
+
     return re.compile(PATTERN_TEMPLATE.substitute(letter=letter, min_occurrences=min_occurrences, max_occurrences=max_occurrences))
+
+def part_one_valid(policy, password):
+    pattern = policy_pattern(policy)
+    return pattern.match(password)
     
 
 def main():
@@ -22,14 +31,11 @@ def main():
 
     for line in input():
         (policy, _, password) = line.partition(': ')
-        pattern = policy_pattern(policy)
-        if pattern.match(password):
+        if part_one_valid(policy, password):
             valid_count += 1
         total_count += 1
 
     print(valid_count)
-
-
 
 if __name__ == '__main__':
     main()
