7a83e7099052102e1afd9e19071a2b28d6836ee2
Robby Stokoe <robbystk@gmail.com>
2020-12-05 10:30:43 -0700

    Added day five part two
    I should have written tests!

--- 04-passport-processing/passports.py
+++ 04-passport-processing/passports.py
@@ -1,14 +1,65 @@
 import sys
 
+import re
+
 def input():
     with open(sys.argv[1]) as f:
         for line in f:
             yield line
 
+def check_year(year_str, min_year, max_year):
+    YEAR_REGEX = re.compile('\d{4}')
+    if not YEAR_REGEX.match(year_str):
+        # print(f"invalid year: {year_str} doesn't have four digits.")
+        return False
+
+    year = int(year_str)
+    if year > max_year or year < min_year:
+        # print(f'invalid year: {year} is not between {min_year} and {max_year}')
+        return False
+
+    return True
+
+def check_height(height_str):
+    matches = re.compile('\A(\d+)(cm|in)\Z').match(height_str)
+    if not matches:
+        return False
+
+    height = int(matches.group(1))
+    suffix = matches.group(2)
+
+    if suffix == 'cm':
+        if 150 <= height and height <= 193:
+            return True
+        else:
+            # print(f"invalid height: {height} isn't between 150 and 193")
+            return False
+    elif suffix == 'in':
+        if 59 <= height and height <= 76:
+            return True
+        else:
+            # print(f"invalid height: {height} isn't between 59 and 76")
+            return False
+    else:
+        return False
+
 def is_valid(passport):
-    REQUIRED_KEYS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    for key in REQUIRED_KEYS:
+    VALIDATORS = {
+            'byr': (lambda v: check_year(v, 1920, 2002)),
+            'iyr': (lambda v: check_year(v, 2010, 2020)),
+            'eyr': (lambda v: check_year(v, 2020, 2030)),
+            'hgt': check_height,
+            'hcl': (lambda v: re.compile('#[0-9a-f]{6}').match(v)),
+            'ecl': (lambda v: v in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}),
+            'pid': (lambda v: re.compile('\A\d{9}\Z').match(v))
+        }
+
+    for key, validator in VALIDATORS.items():
         if key not in passport:
+            # print(f'{key} is missing')
+            return False
+        elif not validator(passport[key]):
+            # print(f'invalid field {key}: {passport[key]}')
             return False
     return True
 
@@ -23,7 +74,7 @@ def main():
                 passport_list[-1][key] = value
 
     # print(passport_list)
-    
+
     valid_count = 0
     for passport in passport_list:
         if is_valid(passport):
