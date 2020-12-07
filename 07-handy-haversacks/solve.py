c02b44ae3105b1c46167bf2caca7963104c82bb7
Robby Stokoe <robbystk@gmail.com>
2020-12-06 23:00:17 -0700

    Add day seven part one

---
+++ 07-handy-haversacks/solve.py
@@ -0,0 +1,60 @@
+import sys
+import re
+
+def input():
+    with open(sys.argv[1]) as f:
+        for line in f:
+            yield line
+
+path_cache = {}
+
+def paths_to_color(rule_list, starting_color, desired_color):
+    rv = []
+    for content in rule_list[starting_color]:
+        color = content[1]
+        if color == desired_color:
+            rv.append([desired_color, starting_color])
+        elif color is None:
+            next
+        else:
+            paths = paths_to_color(rule_list, color, desired_color)
+            for path in paths:
+                path.append(starting_color)
+                rv.append(path)
+    return rv
+
+def main():
+    rule_pattern = re.compile('\A(\d+) (.+) bags?\.?\Z')
+
+    luggage_rules = {}
+    for line in input():
+        outer_color, _, contents = line.strip().partition(' bags contain ')
+        rules = contents.split(', ')
+        contents = []
+        for rule in rules:
+            if rule == 'no other bags.':
+                contents.append((0, None))
+            else:
+                matches = rule_pattern.match(rule)
+                number = matches.group(1)
+                color = matches.group(2)
+                contents.append((number, color))
+
+        luggage_rules[outer_color] = contents
+    
+    # print(luggage_rules)
+
+    desired_color = 'shiny gold'
+
+    paths = []
+    for color in luggage_rules:
+        paths += paths_to_color(luggage_rules, color, desired_color)
+
+    starting_colors = set()
+    for path in paths:
+        starting_colors.add(path[-1])
+
+    print(len(starting_colors))
+
+if __name__ == '__main__':
+    main()
