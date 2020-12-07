535add327574d902c821cb2c2950833b130d00ea
Robby Stokoe <robbystk@gmail.com>
2020-12-06 21:44:18 -0700

    Add day six part one

---
+++ 06-custom-customs/solve.py
@@ -0,0 +1,23 @@
+import sys
+
+def input():
+    with open(sys.argv[1]) as f:
+        for line in f:
+            yield line
+
+def main():
+    group_list = [set()]
+    for line in input():
+        if line == '\n':
+            group_list.append(set())
+        else:
+            group_list[-1] |= set(line.strip())
+
+    count = 0
+    for group in group_list:
+        count += len(group)
+
+    print(count)
+
+if __name__ == '__main__':
+    main()
