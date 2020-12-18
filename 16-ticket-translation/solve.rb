704111c8036bf83555c1bd2d025bb647a747d50b
Robby Stokoe <robbystk@gmail.com>
2020-12-17 17:07:36 -0700

    Add day 16 part two

--- 16-ticket-translation/solve.rb
+++ 16-ticket-translation/solve.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 input = File.open(ARGV[0]) do |f|
   f.readlines.map(&:strip)
 end
@@ -31,16 +33,65 @@ input.each do |line|
   end
 end
 
-ticket_scanning_error_rate = 0
-
-nearby_tickets.each do |ticket|
-  ticket.each do |value|
-    unless rules.values.any? do |ranges|
-            ranges.any? { |range| range.include? value }
-          end
-      ticket_scanning_error_rate += value
+valid_tickets = nearby_tickets.filter do |ticket|
+  ticket.all? do |value|
+    rules.values.any? do |ranges|
+      ranges.any? { |range| range.include? value }
     end
   end
 end
 
-puts ticket_scanning_error_rate
+field_count = rules.length
+field_position_candidates = field_count.times.map { Set.new(rules.keys) }
+determined_indices = Set.new()
+
+ticket_index = 0
+while determined_indices.length < field_count
+  # check for indexes whose field has been determined
+  field_position_candidates.each_with_index do |candidates, index|
+    unless determined_indices.include? index
+      if candidates.length == 1
+        determined_indices.add(index)
+      end
+    end
+  end
+
+  # eliminate fields that have been accounted for from other indexes
+  determined_indices.each do |determined_index|
+    field = field_position_candidates[determined_index].to_a.first
+    field_position_candidates.each_with_index do |candidates, index|
+      next if index == determined_index
+      candidates.delete(field)
+    end
+  end
+
+  # check the next ticket
+  ticket = valid_tickets[ticket_index]
+  field_position_candidates.each.zip(ticket) do |candidates, value|
+    rules.each do |field, ranges|
+      unless ranges.any? { |range| range.include? value }
+        candidates.delete(field)
+      end
+    end
+  end
+  ticket_index += 1
+  if ticket_index >= valid_tickets.length
+    ticket_index = 0
+  end
+
+  # puts field_position_candidates.inspect, determined_indices.inspect
+end
+
+field_positions = field_position_candidates.map { |candidates| candidates.to_a.first }
+
+field_index_map = field_positions.each_with_index.to_h
+
+
+product = 1
+field_index_map.each do |field, index|
+  if field.to_s.start_with? 'departure'
+    product *= my_ticket[index]
+  end
+end
+
+puts product
