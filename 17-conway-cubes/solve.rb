ee7a0c59493e94ceb7e111bf3d2c2d18e185a8d6
Robby Stokoe <robbystk@gmail.com>
2020-12-18 17:44:46 -0700

    Complete day 17 part two

--- 17-conway-cubes/solve.rb
+++ 17-conway-cubes/solve.rb
@@ -11,7 +11,7 @@ def augment_range(range)
 end
 
 class Grid
-  attr_reader :grid, :x_range, :y_range, :z_range
+  attr_reader :grid, :x_range, :y_range, :z_range, :w_range
 
   def initialize(grid: Set.new)
     @grid = grid
@@ -19,7 +19,7 @@ class Grid
   end
 
   def update_ranges
-    limit_array = [[nil, nil], [nil, nil], [nil, nil]]
+    limit_array = [[nil, nil], [nil, nil], [nil, nil], [nil, nil]]
     grid.each do |point|
       point.each_with_index do |coord, i|
         # update minimum
@@ -36,32 +36,35 @@ class Grid
     @x_range = Range.new(limit_array[0][0], limit_array[0][1])
     @y_range = Range.new(limit_array[1][0], limit_array[1][1])
     @z_range = Range.new(limit_array[2][0], limit_array[2][1])
+    @w_range = Range.new(limit_array[3][0], limit_array[3][1])
   end
 
-  def active?(x,y,z)
-    grid.include?([x,y,z])
+  def active?(x,y,z,w)
+    grid.include?([x,y,z,w])
   end
 
-  def inactive(x,y,z)
-    !active?(x,y,z)
+  def inactive(x,y,z,w)
+    !active?(x,y,z,w)
   end
 
-  def activate_cell(x,y,z)
-    grid.add([x,y,z])
+  def activate_cell(x,y,z,w)
+    grid.add([x,y,z,w])
   end
 
-  def inactivate_cell(x,y,z)
-    grid.delete([x,y,z])
+  def inactivate_cell(x,y,z,w)
+    grid.delete([x,y,z,w])
   end
 
   SEARCH_RANGE = -1..1
-  def neighbor_count(x,y,z)
+  def neighbor_count(x,y,z,w)
     count = 0
     SEARCH_RANGE.each do |dz|
       SEARCH_RANGE.each do |dy|
         SEARCH_RANGE.each do |dx|
-          if active?(x+dx, y+dy, z+dz)
-            count += 1
+          SEARCH_RANGE.each do |dw|
+            if active?(x+dx, y+dy, z+dz, w+dw)
+              count += 1
+            end
           end
         end
       end
@@ -78,15 +81,16 @@ class Grid
     augment_range(z_range).each do |z|
       augment_range(y_range).each do |y|
         augment_range(x_range).each do |x|
-          neighbor_count = old_grid.neighbor_count(x,y,z)
-          # puts "#{x}, #{y}, #{z} neighbor count: #{neighbor_count}"
-          if active?(x,y,z)
-            unless STAY_ALIVE_RANGE.include? neighbor_count
-              inactivate_cell(x,y,z)
-            end
-          else
-            if COME_ALIVE_RANGE.include? neighbor_count
-              activate_cell(x,y,z)
+          augment_range(w_range).each do |w|
+            neighbor_count = old_grid.neighbor_count(x,y,z,w)
+            if active?(x,y,z,w)
+              unless STAY_ALIVE_RANGE.include? neighbor_count
+                inactivate_cell(x,y,z,w)
+              end
+            else
+              if COME_ALIVE_RANGE.include? neighbor_count
+                activate_cell(x,y,z,w)
+              end
             end
           end
         end
@@ -100,17 +104,19 @@ class Grid
   end
 
   def to_s
-    z_range.map do |z|
-      "z = #{z}\n" +
-      y_range.map do |y|
-        x_range.map do |x|
-          if grid.include?([x,y,z])
-            '#'
-          else
-            '.'
-          end
-        end.join('')
-      end.join("\n")
+    w_range.map do |w|
+      z_range.map do |z|
+        "z = #{z}, w = #{w}\n" +
+        y_range.map do |y|
+          x_range.map do |x|
+            if grid.include?([x,y,z,w])
+              '#'
+            else
+              '.'
+            end
+          end.join('')
+        end.join("\n")
+      end.join("\n\n")
     end.join("\n\n")
   end
 end
@@ -121,7 +127,7 @@ input.map(&:strip).each_with_index do |line, row|
     case c
     when '.'
     when '#'
-      grid.activate_cell(column, row, 0)
+      grid.activate_cell(column, row, 0, 0)
     else
       raise "invalid input character `#{c.inspect}`"
     end
@@ -131,7 +137,8 @@ grid.update_ranges
 
 puts grid
 
-6.times do
+6.times do |i|
+  puts "cycle #{i}"
   grid.step
 end
 
